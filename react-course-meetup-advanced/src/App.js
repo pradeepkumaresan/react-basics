362a916542bd4b3a38b96516619d7f03d1e7e18c
pradeepkumaresan <pradeepk@thoughtworks.com>
2024-03-11 06:42:07 +0530

    add general layout for the app

--- react-course-meetup-advanced/src/App.js
+++ react-course-meetup-advanced/src/App.js
@@ -1,19 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import AllMeetupsPage from './pages/AllMeetups';
-import FavouritesPage from './pages/Favourites';
-import NewMeetupPage from './pages/NewMeetup';
-import MainNavigation from './components/layout/MainNavigation';
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetup";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Routes>
-        <Route path='/' element={<AllMeetupsPage />}/>
-        <Route path='/new-meetup' element={<NewMeetupPage />} />
-        <Route path='/favourites' element={<FavouritesPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
