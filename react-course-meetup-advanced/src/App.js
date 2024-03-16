976ad515b8950c1bb7b7181027c7ba1a726fefb0
pradeepkumaresan <pradeepk@thoughtworks.com>
2024-03-16 19:51:07 +0530

    revert capability graph component display changes

--- react-course-meetup-advanced/src/App.js
+++ react-course-meetup-advanced/src/App.js
@@ -4,19 +4,17 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetup";
 import Layout from "./components/layout/Layout";
-import CapabilityGraph from "./components/CapabilityGraph";
 
 function App() {
   return (
     <div>
-      <CapabilityGraph />
-      {/* <Layout>
+      <Layout>
         <Routes>
           <Route path="/" element={<AllMeetupsPage />} />
           <Route path="/new-meetup" element={<NewMeetupPage />} />
           <Route path="/favourites" element={<FavouritesPage />} />
         </Routes>
-      </Layout> */}
+      </Layout>
     </div>
   );
 }
